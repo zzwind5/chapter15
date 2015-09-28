aa43f3b3f9c60b2948f80bf95b22a483faedad8b
zjie <zjie@aerohive.com>
2015-09-28 17:59:55 +0800

    add test

--- src/test/java/com/baobaotao/web/UserControllerTest.java
+++ src/test/java/com/baobaotao/web/UserControllerTest.java
@@ -1,6 +1,11 @@
 package com.baobaotao.web;
 
+import java.io.IOException;
+
 import org.junit.Test;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
@@ -15,5 +20,15 @@ public class UserControllerTest {
 		form.add("password", "123456");
 		form.add("realName", "Zhang");
 		rsTemp.postForLocation("http://localhost:8080/chapter15/user/handle41.html", form);
+	}
+	
+	@Test
+	public void handle42() throws IOException {
+		RestTemplate rsTemp = new RestTemplate();
+		byte[] response = rsTemp.postForObject(
+				"http://localhost:8080/chapter15/user/handle42/{imageName}.html", 
+				null, byte[].class, "apple");
+		Resource fileRes = new FileSystemResource("D://apple_bak.jpg");
+		FileCopyUtils.copy(response, fileRes.getFile());
 	}
 }
